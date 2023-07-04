174dfde5e6e71afbc4c8ca0d957f5b8e9afa545c
Vlad Smek <smek1negithub@gmail.com>
2023-07-04 07:22:43 +0300

    Init

---
+++ account/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "email"]
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd["password"]
+
+    def clean_email(self):
+        email = self.cleanded_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidatonError("Email already in use.")
+        return email
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+    def clean_email(self):
+        email = self.cleanded_data["email"]
+        qs = User.objects.exclude(id=self.instance.id).filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("Email already in use")
+        return email
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["date_of_birth", "photo"]
